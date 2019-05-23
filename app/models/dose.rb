a29683780e20867f755f2e8c848a4eaa0d3263f2
Garrett Olson <garrett.olson@enmu.edu>
2019-05-23 15:12:36 +0800

    models connected

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,2 +1,11 @@
 class Dose < ApplicationRecord
+  validates :description, presence: true
+  validates_uniqueness_of :ingredient_id, scope: :cocktail_id
+  belongs_to :cocktail
+  belongs_to :ingredient
 end
+
+
+
+
+

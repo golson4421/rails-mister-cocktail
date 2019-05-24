6151481a456aba790c0e846bcab89a6e89c5b07e
Garrett Olson <garrett.olson@enmu.edu>
2019-05-24 12:28:19 +0800

    begining to add doses controller to be handled in cocktail view

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,20 @@
+class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to cocktail_path(@rcocktail)
+    else
+      render :new
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+require 'json'
+require 'open-uri'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+cocktail_serialized = open(url).read
+cocktail = JSON.parse(cocktail_serialized)
+ # puts cocktail["drinks"]
+ 10.times do
+  cocktail["drinks"].each do |drink|
+    Ingredient.create(name: drink.values)
+  end
+ end
